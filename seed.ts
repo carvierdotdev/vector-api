9f784ad0f200061d04dac4c01119605b53a3d6bc
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-08-07 08:41:33 -0400

    feat: integrate @upstash/vector for batch processing
    •  added @upstash/vector as a dependency in package.json
    
    •  updated seed.ts to use @upstash/vector for batch processing
    
    •  modified pnpm-lock.yaml accordingly
    
    commit-id:c7e7ae2e

--- seed.ts
+++ seed.ts
@@ -1,5 +1,11 @@
+import { Index } from "@upstash/vector";
 import csvParser from "csv-parser";
 import fs from "fs";
+
+const index = new Index({
+  url: process.env.VECTOR_URL,
+  token: process.env.VECTOR_TOKEN,
+});
 
 interface Row {
   text: string;
@@ -28,10 +34,22 @@ async function parseCSV(filePath: string): Promise<Row[]> {
       });
   });
 }
+const STEP = 30;
 
 const seed = async () => {
   const data = await parseCSV("./training_data.csv");
-  console.log(data);
+
+  for (let i = 0; i < data.length; i += STEP) {
+    const chunk = data.slice(i, i + STEP);
+
+    const formatted = chunk.map((row, batchIndex) => ({
+      data: row.text,
+      id: i + batchIndex,
+      metadata: { text: row.text },
+    }));
+
+    await index.upsert(formatted);
+  }
 };
 
 seed();
