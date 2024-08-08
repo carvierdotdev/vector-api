cdb53de76e14a694824a8382c78cd84bf06c534c
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-08-08 08:44:53 -0400

    feat: add initial server setup with Hono
    •  Added src/index.ts with initial server setup using Hono
    
    •  Implemented basic POST endpoint with CORS support
    
    •  Included error handling for JSON body and environment variables
    
    commit-id:aea228c1

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { cors } from "hono/cors";
+const app = new Hono();
+
+type Environment = {
+  VECTOR_URL: string;
+  VECTOR_TOKEN: string;
+};
+
+app.use(cors());
+
+app.post("/", async (c) => {
+  if (c.req.header("Content-Type") !== "application/json") {
+    return c.json(
+      {
+        error: "JSON body expected",
+      },
+      {
+        status: 406,
+      }
+    );
+  }
+
+  try {
+    const { VECTOR_TOKEN, VECTOR_URL } = env<Environment>(c);
+  } catch (error) {
+    console.error(error);
+
+    c.json(
+      {
+        errorMessage: "Oh oh there is an error",
+        error: error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+});
+
+export default app;
